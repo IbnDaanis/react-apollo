74f115d65b18fbd71de5767d7015b4eb3eabf29d
Ikraam <IbnDaanis@Gmail.com>
2021-08-03 03:28:07 +0100

    useGetPosts

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,24 +1,27 @@
 import './app.component.css'
 import '../../common/styles'
 
-import { Header } from '../header/header.component'
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Header } from '../header/header.component'
 import { AboutPage } from '../../pages/about/about.page'
 import { HomePage } from '../../pages/home/home.page'
+import { ApolloProvider } from '@apollo/client'
+import { client } from '../../common/apollo-client'
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path='/about'>
-          <AboutPage />
-        </Route>
-        <Route path='/'>
-          <HomePage />
-        </Route>
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/about'>
+            <AboutPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   )
 }
--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,19 +1,14 @@
-import { Post } from '../../common/interfaces/post.interface'
 import { PostsGrid } from '../../components/posts/posts-grid/posts-grid.component'
+import { useGetPosts } from '../../hooks/useGetPosts'
 import './home.page.css'
 
 export const HomePage: React.FC = () => {
-  const posts: Post[] = [
-    {
-      id: '1',
-      title: 'One',
-      body: 'First body'
-    }
-  ]
+  const posts = useGetPosts()
+  console.log('POSTS: ', posts)
 
   return (
     <div className='home'>
-      <PostsGrid posts={posts} />
+      <PostsGrid posts={posts || []} />
     </div>
   )
 }
